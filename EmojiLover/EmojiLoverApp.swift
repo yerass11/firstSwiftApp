3c702bd9a11d6f6b534a4296fb5d01a4c7de81ad
Yerassyl <Erasylsaiman2005@gmail.com>
2023-04-25 22:31:18 +0600

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Erasyl on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
